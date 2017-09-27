1957a752e281b41558823f16021496c9929165d7
rahul <RAhul.excel2011@gmail.com>
2017-09-27 18:20:14 +0530

    created room working on socket implementation

--- client/src/app/startChat/startChat.component.ts
+++ client/src/app/startChat/startChat.component.ts
@@ -1,17 +1,35 @@
 import {
 	Component,
-	OnInit
+	OnInit,
+	OnDestroy
 } from '@angular/core';
 import {ApiService} from '../Apiservice/apiservice';
+import {chatService} from '../Apiservice/chatservice';
 @Component ({
 	selector:'start-chat',
 	templateUrl:'./startChat.html',
 	styleUrls:['./startChat.css'],
-	providers:[ApiService]
+	providers:[ApiService,chatService]
 	
 })
-export class startChatComponent{
-	constructor(private apiservice:ApiService){}
+export class startChatComponent implements OnInit {
+	messages=[];
+	connection;
+	message;
+	constructor(private apiservice:ApiService, private chatservice:chatService){}
 	users: any;
-	
+	sendMessage(){
+		console.log(">>>>>>>>>>>>>>>message",this.message);
+    this.chatservice.sendMessage(this.message);
+    this.message = '';
+  }
+  ngOnInit() {
+    this.connection = this.chatservice.getMessages().subscribe(message => {
+      this.messages.push(message);
+    })
+  }
+  // Let's unsubscribe our Observable
+  ngOnDestroy() {
+    this.connection.unsubscribe();
+  }
 }
